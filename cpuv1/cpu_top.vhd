e445d32a1ddc36b43641b74414b88455889e9887
ksk <none@none>
2009-10-06 14:22:30 +0900

    Add CPU Top Module

---
+++ cpuv1/cpu_top.vhd
@@ -0,0 +1,31 @@
+-- @module : cpu_top
+-- @author : ksk
+-- @date   : 2009/10/06
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity cpu_top is 
+port (
+    clk			: in	  std_logic
+    
+    
+    ); 
+     
+end cpu_top;     
+        
+
+architecture synth of cpu_top is
+               
+begin  
+
+end synth;
+
+
+
+
+
+
+
+
