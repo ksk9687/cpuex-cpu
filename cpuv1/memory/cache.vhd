29ed89c35ee0c6bef0cec3363881ca02150bc6aa
ksk <none@none>
2009-10-22 02:38:43 +0900

    DCacheの大きさをgenericで指定できるようにした。 DCacheを大きくして実行してみた。150.188sec

--- cpuv1/memory/cache.vhd
+++ cpuv1/memory/cache.vhd
@@ -65,6 +65,10 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity dcache is
+	generic (
+		width : integer := 9;
+		depth : integer := 2048
+	);
 	port  (
 		clk : in std_logic;
 		address: in std_logic_vector(19 downto 0);
@@ -76,30 +80,30 @@ entity dcache is
 end dcache;
 
 architecture arch of dcache is
-    type cache_tag_type is array (0 to 1023) of std_logic_vector (10 downto 0); --10 + 1
-    type cache_data_type is array (0 to 1023) of std_logic_vector (31 downto 0); --32
+    type cache_tag_type is array (0 to depth - 1) of std_logic_vector (width downto 0); --width + 1
+    type cache_data_type is array (0 to depth - 1) of std_logic_vector (31 downto 0); --32
     
     signal cache : cache_tag_type :=(
-    	others => "0"&"0000000000"
+    	others => (others => '0')
     	);
     	
    signal cache_data : cache_data_type;
    
-    signal entry : std_logic_vector(10 downto 0) := "00000000000";
-    signal read_addr : std_logic_vector(9 downto 0) := (others => '0');
+    signal entry : std_logic_vector(width downto 0) := (others => '0');
+    signal read_addr : std_logic_vector((width - 1) downto 0) := (others => '0');
     signal read : std_logic_vector(31 downto 0) := (others => '0');
 begin
 	read_data <= read;
-	hit <= entry(10) when entry(9 downto 0) = address(19 downto 10) else '0';
+	hit <= entry(width) when entry((width - 1) downto 0) = address(19 downto (20 - width)) else '0';
 	--hit <= '0';
 	
 	process (clk)
 	begin
 	    if rising_edge(clk) then
 	        if set = '1' then
-	            cache(conv_integer(address(9 downto 0))) <= '1'&address(19 downto 10);
+	            cache(conv_integer(address((19 - width) downto 0))) <= '1'&address(19 downto (20 - width));
 	        end if;
-				entry <= cache(conv_integer(address(9 downto 0)));
+				entry <= cache(conv_integer(address((19 - width) downto 0)));
 	    end if;
 	end process;
 	
@@ -107,9 +111,9 @@ begin
 	begin
 	    if rising_edge(clk) then
 	        if set = '1' then
-	            cache_data(conv_integer(address(9 downto 0))) <= set_data;
+	            cache_data(conv_integer(address((19 - width) downto 0))) <= set_data;
 	        end if;
-	        read <= cache_data(conv_integer(address(9 downto 0)));
+	        read <= cache_data(conv_integer(address((19 - width)downto 0)));
 	    end if;
 	end process;
 	
