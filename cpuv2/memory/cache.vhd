5dc4be45fb301b7f4e1a7dba1b7cfd4058879b69
ksk <none@none>
2009-11-29 02:09:00 +0900

    制約をクリア。

--- cpuv2/memory/cache.vhd
+++ cpuv2/memory/cache.vhd
@@ -68,7 +68,7 @@ entity dcache is
 	generic (
 		width : integer := 9;
 		depth : integer := 2048;
-		check_width : integer := 4
+		check_width : integer := 2
 	);
 	port  (
 		clk,clkfast : in std_logic;
@@ -94,13 +94,20 @@ architecture arch of dcache is
     signal entry,entry_buf : std_logic_vector(width downto 0) := (others => '0');
     signal read_addr : std_logic_vector((width - 1) downto 0) := (others => '0');
     signal read : std_logic_vector(31 downto 0) := (others => '0');
-    signal hit_buf,hit_p,read_f_buf,cmp,cmp_buf : std_logic := '0';
+    signal hit_buf,hit_p,read_f_buf: std_logic := '0';
+    signal cmp,cmp_buf :std_logic_vector(4 downto 0) := "00000";
     signal address_buf : std_logic_vector(19 downto 0) := (others => '0');
 begin
 	read_data <= read;
-	hit <= ((hit_buf and cmp_buf)or read_f_buf) when entry_buf((width - 1) downto (check_width)) = address_buf(19 downto (20 - width + check_width)) else read_f_buf;
-	hit_p <= (entry(width)) ;
-	cmp <= '1' when entry((check_width - 1) downto 0) = address((20 - 1 - width + check_width) downto (20 - width)) else '0';
+	hit <= (read_f_buf) or (cmp_buf(0) and cmp_buf(1) and cmp_buf(2) and cmp_buf(3) and cmp_buf(4));
+	 
+	--hit_p <= (entry(width)) ;
+	
+	cmp(0) <= not( (entry(0) xor address(11)) or (entry(1) xor address(12)) );
+	cmp(1) <= not( (entry(2) xor address(13)) or (entry(3) xor address(14)) );
+	cmp(2) <= not( (entry(4) xor address(15)) or (entry(5) xor address(16)) );
+	cmp(3) <= not( (entry(6) xor address(17)) or (entry(7) xor address(18)) );
+	cmp(4) <= (not (entry(8) xor address(19))) and entry(9);
 
 	--hit <= '0';
 	-- 
@@ -109,7 +116,7 @@ begin
 	    if rising_edge(clk) then
 	    	read_f_buf <= not read_f;
 	    	entry_buf <= entry;
-	    	hit_buf <= hit_p;
+	    	--hit_buf <= hit_p;
 	    	cmp_buf <= cmp;
 	    	address_buf <= address;
 	    end if;
