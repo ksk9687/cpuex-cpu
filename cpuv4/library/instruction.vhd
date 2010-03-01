fa582a5760ba2db38291ae478e6e2f6077e71857
ksk <none@none>
2010-03-02 04:04:39 +0900

    ISA調整

--- cpuv4/library/instruction.vhd
+++ cpuv4/library/instruction.vhd
@@ -31,58 +31,28 @@ package instruction is
    constant op_load :	std_logic_vector(5 downto 0) := o"20";
    constant op_loadr :	std_logic_vector(5 downto 0) := o"21";
    constant op_store:	std_logic_vector(5 downto 0) := o"22";
-   constant op_fload :	std_logic_vector(5 downto 0) := o"20";
-   constant op_floadr :	std_logic_vector(5 downto 0) := o"21";
-   constant op_fstore:	std_logic_vector(5 downto 0) := o"22";
+   constant op_fload :	std_logic_vector(5 downto 0) := o"24";
+   constant op_floadr :	std_logic_vector(5 downto 0) := o"25";
+   constant op_fstore:	std_logic_vector(5 downto 0) := o"26";
+   constant op_itof :	std_logic_vector(5 downto 0) := o"23";
+   constant op_ftoi:	std_logic_vector(5 downto 0) := o"27";
    
-   constant op_load :	std_logic_vector(5 downto 0) := o"20";
-   constant op_loadr :	std_logic_vector(5 downto 0) := o"31";
-   constant op_load :	std_logic_vector(5 downto 0) := o"20";
-   constant op_store_inst:	std_logic_vector(5 downto 0) := o"23";
+   constant op_read	:	std_logic_vector(5 downto 0) := o"30";
+   constant op_write:	std_logic_vector(5 downto 0) := o"32";
+   constant op_led	:	std_logic_vector(5 downto 0) := o"34";
+   constant op_ledi	:	std_logic_vector(5 downto 0) := o"36";
    
-   constant op_hsread :	std_logic_vector(5 downto 0) := o"40";
-   constant op_hswrite :std_logic_vector(5 downto 0) := o"41";
-   constant op_mv :		std_logic_vector(5 downto 0) := o"05";
+   constant op_nop	:	std_logic_vector(5 downto 0) := o"40";
    
-   constant op_read	:	std_logic_vector(5 downto 0) := o"50";
-   constant op_write:	std_logic_vector(5 downto 0) := o"51";
-   constant op_led	:	std_logic_vector(5 downto 0) := o"52";
-   constant op_ledi	:	std_logic_vector(5 downto 0) := o"53";
+   constant op_cmpjmp	:	std_logic_vector(5 downto 0) := o"61";
+   constant op_cmpjmp	:	std_logic_vector(5 downto 0) := o"60";
+   constant op_cmpijmp	:	std_logic_vector(5 downto 0) := o"64";
+   constant op_cmpijmp	:	std_logic_vector(5 downto 0) := o"65";
+   constant op_cmpfjmp	:	std_logic_vector(5 downto 0) := o"70";
+   constant op_cmpfjmp	:	std_logic_vector(5 downto 0) := o"71";
    
-   constant op_nop	:	std_logic_vector(5 downto 0) := o"60";
-   constant op_halt	:	std_logic_vector(5 downto 0) := o"61";
-   constant op_sleep:	std_logic_vector(5 downto 0) := o"62";
-   
-   constant op_jmp	:	std_logic_vector(5 downto 0) := o"70";
-   constant op_jal	:	std_logic_vector(5 downto 0) := o"71";
-   constant op_jr	:	std_logic_vector(5 downto 0) := o"72"; 	
-
-
-   constant alui_op_li		:	std_logic_vector(2 downto 0) := o"0";
-   constant alui_op_addi	:	std_logic_vector(2 downto 0) := o"1";
-   constant alui_op_sll		:	std_logic_vector(2 downto 0) := o"2";
-   constant alui_op_cmpi	:	std_logic_vector(2 downto 0) := o"3";
-   constant alui_op_mv	:	std_logic_vector(2 downto 0) 	 := o"5";
-   constant alui_op_fabs	:	std_logic_vector(2 downto 0) := o"6";
-   constant alui_op_fneg	:	std_logic_vector(2 downto 0) := o"7";
-   
-	constant alu_op_add	: std_logic_vector(2 downto 0) := o"0";
-	constant alu_op_sub	: std_logic_vector(2 downto 0) := o"1";
-	constant alu_op_cmp	: std_logic_vector(2 downto 0) := o"2";
-	
-	constant iou_op_read	: std_logic_vector(2 downto 0) := o"0";
-	constant iou_op_write	: std_logic_vector(2 downto 0) := o"1";
-	constant iou_op_led	: std_logic_vector(2 downto 0) := o"2";
-	constant iou_op_ledi	: std_logic_vector(2 downto 0) := o"3";
-	
-	constant lsu_op_load	: std_logic_vector(2 downto 0) := o"0";
-	constant lsu_op_loadr	: std_logic_vector(2 downto 0) := o"1";
-	constant lsu_op_store	: std_logic_vector(2 downto 0) := o"2";
-	constant lsu_op_store_inst	: std_logic_vector(2 downto 0) := o"3";
-	
-	
-	constant sp_op_nop	: std_logic_vector(2 downto 0) := o"0";
-
+   constant op_jal	:	std_logic_vector(5 downto 0) := o"74";
+   constant op_jr	:	std_logic_vector(5 downto 0) := o"76"; 	
 end package instruction;  
  
 
